7919a40a054816a2262ec022aa4a1084680ab30d
danielgomezperez <daniobey24@gmail.com>
2023-03-05 22:54:19 +0100

    Squash de la rama raíces realizado

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,28 +1,6 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
-public class Calculadora {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Operaciones operaciones = new Operaciones();
 
-
-        /** Probamos que el usuario introduzca datos correctos (números), para poder operar con ellos**/
-        try{ System.out.println("Indica qué operación quieres realiza:\n 1.- Suma\n 2.- Resta\n 3.- Multiplicación\n 4.- División\n5.- Raíz n-ésima");
-            final int operacion = sc.nextInt();
-            System.out.println("Dame el primer número");
-            final double  n1 = sc.nextInt();
-            System.out.println("Dame el segundo número");
-            final double  n2 = sc.nextInt();
-            final double resultado = operaciones.operacion(n1,n2,operacion);
-            if (resultado != 0.000000143546456543234212) {
-                System.out.println("El resultado es " + resultado);
-            }
-
-
-
-        }catch(Exception ex){
-            System.out.println("Por favor, introduzca una cifra");
-        }
 
 
 
